8bbf8986d85d3ffed440e9844622edc6ffb142cc
Pulkit Singhal <pulkit@shoppinpal.com>
2015-04-14 11:57:58 -0500

    add a model for lineitems

--- common/models/stock-order-lineitem-model.js
+++ common/models/stock-order-lineitem-model.js
@@ -1,15 +1,15 @@
 'use strict';
 var Promise = require('bluebird');
 
-module.exports = function(ReportModel) {
+module.exports = function(StockOrderLineitemModel) {
 
   // https://github.com/strongloop/loopback/issues/418
   // once a model is attached to the data source
-  ReportModel.on('dataSourceAttached', function(obj){
+  StockOrderLineitemModel.on('dataSourceAttached', function(obj){
     // wrap the whole model in Promise
     // but we need to avoid 'validate' method
-    ReportModel = Promise.promisifyAll(
-      ReportModel,
+    StockOrderLineitemModel = Promise.promisifyAll(
+      StockOrderLineitemModel,
       {
         filter: function(name, func, target){
           return !( name == 'validate');
--- common/models/user-model.js
+++ common/models/user-model.js
@@ -1,15 +1,14 @@
-'use strict';
 var Promise = require('bluebird');
 
-module.exports = function(ReportModel) {
+module.exports = function(UserModel) {
 
   // https://github.com/strongloop/loopback/issues/418
   // once a model is attached to the data source
-  ReportModel.on('dataSourceAttached', function(obj){
+  UserModel.on('dataSourceAttached', function(obj){
     // wrap the whole model in Promise
     // but we need to avoid 'validate' method
-    ReportModel = Promise.promisifyAll(
-      ReportModel,
+    UserModel = Promise.promisifyAll(
+      UserModel,
       {
         filter: function(name, func, target){
           return !( name == 'validate');
