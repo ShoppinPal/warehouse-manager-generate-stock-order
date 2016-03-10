dd4b85f82428f70dcb22b7bee0c4ef95d801c027
Yash Gandhi <ysg410@gmail.com>
2016-03-10 19:13:52 +0530

    Worker to list number of products to delete from Vend with zero sales in specified time, zero inventory and not on consignments

--- product-clean-up.js
+++ product-clean-up.js
@@ -9,10 +9,11 @@ try {
   var _ = require('underscore');
   var vendSdk = require('vend-nodejs-sdk')({});
   var moment = require('moment');
+  var loopback_filter = require('loopback-filters');
 
-  // Global variable for logging
+    // Global variable for logging
   var commandName = path.basename(__filename, '.js'); // gives the filename without the .js extension
-
+  var consignmentProductsById = [];
   var params = null;
   var task_id = null;
   var config = null;
@@ -31,7 +32,6 @@ try {
       task_id = process.argv[index + 1];
     }
   });
-
   console.log(commandName, 'params:', params);
   console.log(commandName, 'config:', config);
   console.log(commandName, 'task_id:', task_id);
@@ -92,9 +92,8 @@ try {
           return Promise.resolve(params)
             .then(function (params) { // (1) create a report if params.reportId is empty
 
-                /*console.log(commandName, 'WTF');
+                console.log(commandName, 'WTF');
                 console.log(commandName, 'report already exists');
-                params.loopbackAccessToken = params.credentials;
                 // set the access token to be used for all future invocations
                 console.log(commandName, 'params.loopbackAccessToken.id', params.loopbackAccessToken.id);
                 console.log(commandName, 'params.loopbackAccessToken.userId', params.loopbackAccessToken.userId);
@@ -104,44 +103,84 @@ try {
                 };
                 console.log(commandName, 'the access token to be used for all future invocations has been set');
 
-                return Promise.resolve();*/
+                return Promise.resolve();
+            })
+            .then(function getConsignments(){
+                //get consignments to find products on the consignment for maximum 2 weeks
+
+
+                var connectionInfo = utils.loadOauthTokens();
+                var consignments = vendSdk.consignments.stockOrders.fetchAll(connectionInfo)
+
+                .then(function (consignments){
+                  var consignmentsById = [];
+
+                  var now = moment().format('YYYY-MM-DD');
+                  var indexFortoday = moment().day();
+                  var getMeToThisSunday = moment().day("Sunday").format('YYYY-MM-DD');
+                  var getMeToLastSunday_NoMatterWhatTheCurrentDayOfTheWeekIs = moment().day(-7).format('YYYY-MM-DD');
+                  var dateFilter = {where : {consignment_date : {gt : getMeToLastSunday_NoMatterWhatTheCurrentDayOfTheWeekIs}}};
+
+                  var sentOrOpenFilter = {where: {status:'OPEN'}};// {inq: ['OPEN', 'SENT']}}};
+                  var cleanedConsignments = loopback_filter(consignments,sentOrOpenFilter);
+                  var filteredDateConsignments = loopback_filter(cleanedConsignments,dateFilter);
+
+                  filteredDateConsignments.forEach(function(consignment){
+                    //console.log(consignment.status);
+                    consignmentsById.push(consignment.id);
+                                        
+                  })
+                  console.log(commandName, 'consignmentsById.length : ',consignmentsById.length);
+                  var argsForConsignments = {page: { required: false, key: 'page', value: undefined },
+                                               pageSize: { required: false, key: 'page_size', value: undefined },
+                                               consignmentIdIndex: {required: false,key:'consignment_index',value:undefined},
+                                               consignmentIds: {required:true,key:'consignment_id',value:consignmentsById} 
+                                              };
+                    var consignmentProducts = vendSdk.consignments.products.fetchAllForConsignments(argsForConsignments,connectionInfo)
+                    .then(function getConsignmentProducts(consignmentProducts){
+                      
+                      
+                      consignmentProducts.forEach(function(singleConsignment){
+                        if(!(consignmentIdExists(singleConsignment.product_id,consignmentProductsById)))
+                        {
+                          consignmentProductsById.push(singleConsignment.product_id);
+                        }
+                      })  
+                      console.log(commandName, 'consignmentProductsById.length : ',consignmentProductsById.length);
+
+                      function consignmentIdExists(productId,array) {
+                      var i = null;
+                      for (i = 0; array.length > i; i += 1) {
+                            if (array[i].product_id === productId) {
+                                return true;
+                            }
+                        }
+                         
+                        return false;
+                      };                                          
+
+                })
+                
+
+
+              })
             })
             .then(function getSales(){
               //check for zero sales
 
 
-              var aWeekAgo = moment.utc().subtract(4, 'months');
-              var twoWeeksAgo = moment.utc().subtract(2, 'weeks');
-              var aMonthAgo = moment.utc().subtract(4, 'weeks');
-              var sixWeeksAgo = moment.utc().subtract(6, 'weeks');
-              var twoMonthsAgo = moment.utc().subtract(8, 'weeks');
-              var intervalOptions = [
-                aWeekAgo,
-                twoWeeksAgo,
-                aMonthAgo,
-                sixWeeksAgo,
-                twoMonthsAgo
-              ];
-              
-              var intervalOptionsForDisplay = [
-                  'Starting a week ago (' + aWeekAgo.format('YYYY-MM-DD') + ')',
-                  'Starting two weeks ago (' + twoWeeksAgo.format('YYYY-MM-DD') + ')',
-                  'Starting a month ago (' + aMonthAgo.format('YYYY-MM-DD') + ')',
-                  'Starting six weeks ago (' + sixWeeksAgo.format('YYYY-MM-DD') + ')',
-                  'Starting two months ago (' + twoMonthsAgo.format('YYYY-MM-DD') + ')'
-              ];
-
-              
+              //var sinceDate = moment.utc().subtract(3, 'months').format('YYYY-MM-DD');
               var argsForSales = vendSdk.args.sales.fetch();
-              var sinceDate = aWeekAgo.format('YYYY-MM-DD');
+              //console.log(argsForSales);
+              var sinceDate = params.reportDate;
               argsForSales.since.value = sinceDate;
               //argsForSales.outletApiId.value = params.outletId;
               var connectionInfo = utils.loadOauthTokens();
-              console.log(connectionInfo);
+              //console.log(connectionInfo);
 
               return vendSdk.sales.fetchAll(argsForSales,connectionInfo)
               .then(function(registerSales){
-                  //console.log(commandName + ' > Showing sales :  '+registerSales[0].sale_date);   
+                  
                   return Promise.resolve(registerSales);    
                 });
 
@@ -150,66 +189,86 @@ try {
             .tap(function filterSales(registerSales) {
           
               var connectionInfo = utils.loadOauthTokens();
-              console.log(connectionInfo);
-              console.log("Register Sale : "+ registerSales.length);
-              return Promise.map(registerSales,function(){
+              //console.log(connectionInfo);
+              console.log(commandName, 'Register Sale : '+ registerSales.length);
                 registerSales.forEach(function(singleSale){
                       var singleProductSale = singleSale.register_sale_products;
                       singleProductSale.forEach(function(sale){
                           filteredSales.push(sale);
-                          console.log(sale);
+                          //console.log(sale);
                       })
                     })
-              },
-              {concurrency: 1});
-              
+
+              return Promise.resolve(filteredSales);
           
             })
-            .then(function findProducts()
+          .then(function findProducts()
             {
+              //get all products information
+
               var connectionInfo = utils.loadOauthTokens();
               return vendSdk.products.fetchAll(connectionInfo)
-              .then(function(products){
+
+              .then(function(products)
+              {
                 
-                console.log("Filtered sale : " + filteredSales.length);
+                
+                console.log(commandName,'Filtered sale : ' + filteredSales.length);
                 var dilutedSales = [];
                 filteredSales.forEach(function(singleSale){
                   //console.log("Filtered product name : "+ singleSale.name);
-                  if((!(filterExists(singleSale.product_id,dilutedSales))))
+                  if((!(salesIdExists(singleSale.product_id,dilutedSales))))
                   {
-                    //console.log("Diluted product name : " + singleSale.name);
                     dilutedSales.push(singleSale);
                   }
                 })
-                console.log("Diluted sale : " + dilutedSales.length);
-                //console.log(dilutedSales);
-                
-                products.forEach(function(product){
-                  if(!(filterExists(product.id,dilutedSales)))
+                console.log(commandName, 'Diluted sale : ' + dilutedSales.length);
+
+                var singleProduct = null;      
+                var i=0,j=0;
+                var sum = parseInt(0);
+                var undefinedFilter = {where: {inventory: {neq :undefined} }};
+                var cleanedProducts = loopback_filter(products,undefinedFilter);
+
+                  cleanedProducts.forEach(function(product){
+                  sum = 0;
+                  if(!(salesIdExists(product.id,dilutedSales)))
                   {
                     
-
-                    if(product.name == "Discount"){
-
-                    }
-                    else
+                    var singleProductInventory = product.inventory;
+                    singleProductInventory.forEach(function(inv){
+                    
+                      sum = parseInt(sum) + parseInt(inv.count);
+                    });
+                    
+                  if(sum == parseInt(0))
+                  {
+                    if(!(consignmentIdExists(product.id,consignmentProductsById)))
                     {
-                      var singleProductInventory = product.inventory;
-                      singleProductInventory.forEach(function(inv){
-                        if(inv.count == 0.00000)
-                        {
-                          if(!(deletionExists(product,deleteFromVend)))
-                          {
-                            console.log("Zero inventory : " + product.name);
-                            deleteFromVend.push(product);
+                      if(product.supplier_name != 'FFCC') {
+                          if (!(productsIdExists(product.id, deleteFromVend))) {
+                              deleteFromVend.push(product);
+                              j++;
                           }
-                        }
-                      });
+                      }
                     }
+                   }
                   }
                 })
+                
+                function consignmentIdExists(productId,array) {
+                    var i = null;
+                    for (i = 0; array.length > i; i += 1) {
+                        if (array[i] === productId) {
+                            return true;
+                        }
+                    }
+                     
+                    return false;
+                };  
 
-                function filterExists(productId,array) {
+
+                function salesIdExists(productId,array) {
                     var i = null;
                     for (i = 0; array.length > i; i += 1) {
                         if (array[i].product_id === productId) {
@@ -218,29 +277,26 @@ try {
                     }
                      
                     return false;
-                };    
-                function deletionExists(obj, objs)
-                {
-                    var objStr = JSON.stringify(obj);
+                };  
 
-                    for(var i=0;i<objs.length; i++)
-                    {
-                        if(JSON.stringify(objs[i]) == objStr)
-                        {
-                            return 1;
+                function productsIdExists(productId,array) {
+                    var i = null;
+                    for (i = 0; array.length > i; i += 1) {
+                        if (array[i].id === productId) {
+                            return true;
                         }
                     }
+                     
+                    return false;
+                };  
 
-                    return 0;
-                }            
-                console.log("Deletion count : " + deleteFromVend.length);
-
+                console.log(commandName, 'Deletion count : ' + deleteFromVend.length);
             })
           })
           
           .catch(function (error) {
               console.error('2nd last dot-catch block');
-              console.log(commandName, 'ERROR', error);
+              console.log(commandName, 'ERROR', error.stack);
               return Promise.reject(error);
             });
         }
@@ -248,23 +304,27 @@ try {
         {
           console.error('3rd last catch block');
           console.error(commandName, e);
+          console.log(commandName, 'ERROR', e.stack);
           // TODO: throw or float up promise chain or just exit the worker process here?
         }
       })
       .catch(function (error) {
         console.error('last dot-catch block');
         console.log(commandName, 'ERROR', error);
+        console.log(commandName, 'ERROR', error.stack);
         process.exit(FAILURE); // this is the last one so exit the worker process
       });
   }
   catch (e) {
     console.error('2nd last catch block');
     console.error(commandName, e);
+    console.log(commandName, 'ERROR', e.stack);
     // TODO: throw or float up promise chain or just exit the worker process here?
   }
 }
 catch (e) {
     console.error('Last catch block');
     console.error(commandName, e);
+    console.log(commandName, 'ERROR', e.stack);
     // TODO: throw or float up promise chain or just exit the worker process here?
 }
